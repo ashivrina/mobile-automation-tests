63ef46fe7578cd63d0eb9c9f202ccacc25fc6040
annashv <annashivrina@gmail.com>
2021-11-27 15:57:07 +0100

    Small fixes

--- src/test/java/configs/AndroidTestsConfig.java
+++ src/test/java/configs/AndroidTestsConfig.java
@@ -7,36 +7,25 @@ import static org.aeonbits.owner.Config.*;
 @Sources({"classpath:${deviceHost}.properties"})
 public interface AndroidTestsConfig extends Config {
 
-    @Key("user")
     String user();
 
-    @Key("key")
     String key();
 
-    @Key("deviceUrl")
     String deviceUrl();
 
-    @Key("app")
     String app();
 
-    @Key("device")
     String device();
 
-    @Key("osVersion")
     String oSVersion();
 
-    @Key("uiautomator2ServerInstallTimeout")
     String uiautomator2ServerInstallTimeout();
 
-    @Key("appPackage")
     String appPackage();
 
-    @Key("appActivity")
     String appActivity();
 
-    @Key("platformName")
     String platformName();
 
-    @Key("apkUrl")
     String apkUrl();
 }
